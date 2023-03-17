242ecfc3355d4f081e9a0e2d47cc04f7b0f1b53a
Nguyễn Ngọc Hoàng <hoang122003k@gmail.com>
2023-03-17 14:49:37 +0700

    main

--- src/lab1/sort.ts
+++ src/lab1/sort.ts
@@ -1,41 +1,22 @@
-// const Arr1=[1,3,5,7,9]
-// const Arr2=[2,4,6,8,10]
+const arrNum3 = [1,9,5,7,3];
 
-// function Sort<H>(arr:H[],callback?:(a:H[],b:H[])=>H){
-//     for(let i=0;i<arr.length-1;i++){
-//         for(let j=i+1;j<arr.length;j++){
-//             if(arr[i]<arr[j]){
-//                 let temp=arr[i]
-//                 arr[i]=arr[j]
-//                 arr[j]=temp
-//             }
-//         }
-//     }
-// }
-// Sort(Arr1);
-// console.log(Arr1);
-
-
-
-const arrSort1 = [2,9,10,6,7];
-const arrSort2 = ["d","a","f","v"];
-function labSort<T> (arr1:T[],callback:(a:T,b:T)=>T){
-for(let i = 0;i<arr1.length-1;i++){
-for(let j = i+1;j<arr1.length;j++){
-    if(arr1[i]<arr1[j]){
-        let temp = arr1[i]
-        arr1[i] = arr1[j]
-        arr1[j] = temp
+const sortLab = function (
+  array: number[],
+  compare?: (a: number, b: number) => number
+) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (compare(array[i], array[j]) < 0) {
+        let x = array[i];
+        array[i] = array[j];
+        array[j] = x;
+      }
     }
-    let temp1 = [];
-    const newItem = callback(arr1[i],arr1[j]);
-    temp1.push(newItem)
-    return temp1
-}
-    }
-}
-const taingu = labSort(arrSort1,(a,b)=>{
- return a -b;
-})
+  }
+};
 
-console.log(taingu);
+sortLab(arrNum3, (a, b) => a - b);
+console.log(arrNum3);
+
+
+
